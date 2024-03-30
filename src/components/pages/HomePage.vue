eccb1676ffe6fca6c7ded8c39aa149480705bf3e
minhtuan12c9 <hophanminhtuan2619@gmail.com>
2024-03-30 15:42:11 +0700

    build Home Page, Header and SideBar

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,12 +1,303 @@
-<template>HOME PAGE</template>
+<template>
+  <Header />
+  <SideBar />
+  <div>
+    <main id="main" class="main">
+      <div class="truyendecu">
+        <div class="pagetitle">
+          <h1 class="fw-bold">TRUYỆN ĐỀ CỬ</h1>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <!-- Slides with captions -->
+            <div
+              id="carouselExampleCaptions"
+              class="carousel slide carousel-small mt-3"
+              data-bs-ride="carousel"
+            >
+              <div class="carousel-indicators">
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleCaptions"
+                  data-bs-slide-to="0"
+                  class="active"
+                  aria-current="true"
+                  aria-label="Slide 1"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleCaptions"
+                  data-bs-slide-to="1"
+                  aria-label="Slide 2"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleCaptions"
+                  data-bs-slide-to="2"
+                  aria-label="Slide 3"
+                ></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    src="assets/img/slides-1.jpg"
+                    class="d-block w-100"
+                    alt="..."
+                  />
+                  <div class="carousel-caption d-none d-md-block">
+                    <h1 class="fw-bold">SOLO LEVELING</h1>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img
+                    src="assets/img/slides-2.jpg"
+                    class="d-block w-100"
+                    alt="..."
+                  />
+                  <div class="carousel-caption d-none d-md-block">
+                    <h1 class="fw-bold">ELECEED</h1>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img
+                    src="assets/img/slides-3.jpg"
+                    class="d-block w-100"
+                    alt="..."
+                  />
+                  <div class="carousel-caption d-none d-md-block">
+                    <h1 class="fw-bold">ONE PUNCH MAN</h1>
+                  </div>
+                </div>
+              </div>
+
+              <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+            <!-- End Slides with captions -->
+          </div>
+        </div>
+      </div>
+
+      <div class="truyenmoicapnhat">
+        <div class="pagetitle">
+          <h1 class="fw-bold">TRUYỆN MỚI CẬP NHẬT</h1>
+        </div>
+        <!-- list item -->
+        <div class="d-flex flex-wrap" style="gap: 10px">
+          <!-- item -->
+          <div class="card">
+            <div
+              class="card-body col-lg-2 mt-3"
+              style="width: calc((1543px - 40px) / 5)"
+            >
+              <img
+                src="assets/img/card.jpg"
+                class="card-img-top"
+                alt="..."
+                style="height: 300px"
+              />
+              <a class="card-text text-center d-block mt-2">
+                Cốt binh trở lại
+              </a>
+              <div class="d-flex justify-content-between mt-2">
+                <a>Chap 01</a>
+                <span>10 phút trước</span>
+              </div>
+            </div>
+          </div>
+          <!-- End item -->
+          <!-- item -->
+          <div class="card">
+            <div
+              class="card-body col-lg-2 mt-3"
+              style="width: calc((1543px - 40px) / 5)"
+            >
+              <img
+                src="assets/img/card.jpg"
+                class="card-img-top"
+                alt="..."
+                style="height: 300px"
+              />
+              <a class="card-text text-center d-block mt-2">
+                Cốt binh trở lại
+              </a>
+              <div class="d-flex justify-content-between mt-2">
+                <a>Chap 01</a>
+                <span>10 phút trước</span>
+              </div>
+            </div>
+          </div>
+          <!-- End item -->
+          <!-- item -->
+          <div class="card">
+            <div
+              class="card-body col-lg-2 mt-3"
+              style="width: calc((1543px - 40px) / 5)"
+            >
+              <img
+                src="assets/img/card.jpg"
+                class="card-img-top"
+                alt="..."
+                style="height: 300px"
+              />
+              <a class="card-text text-center d-block mt-2">
+                Cốt binh trở lại
+              </a>
+              <div class="d-flex justify-content-between mt-2">
+                <a>Chap 01</a>
+                <span>10 phút trước</span>
+              </div>
+            </div>
+          </div>
+          <!-- End item -->
+          <!-- item -->
+          <div class="card">
+            <div
+              class="card-body col-lg-2 mt-3"
+              style="width: calc((1543px - 40px) / 5)"
+            >
+              <img
+                src="assets/img/card.jpg"
+                class="card-img-top"
+                alt="..."
+                style="height: 300px"
+              />
+              <a class="card-text text-center d-block mt-2">
+                Cốt binh trở lại
+              </a>
+              <div class="d-flex justify-content-between mt-2">
+                <a>Chap 01</a>
+                <span>10 phút trước</span>
+              </div>
+            </div>
+          </div>
+          <!-- End item -->
+          <!-- item -->
+          <div class="card">
+            <div
+              class="card-body col-lg-2 mt-3"
+              style="width: calc((1543px - 40px) / 5)"
+            >
+              <img
+                src="assets/img/card.jpg"
+                class="card-img-top"
+                alt="..."
+                style="height: 300px"
+              />
+              <a class="card-text text-center d-block mt-2">
+                Cốt binh trở lại
+              </a>
+              <div class="d-flex justify-content-between mt-2">
+                <a>Chap 01</a>
+                <span>10 phút trước</span>
+              </div>
+            </div>
+          </div>
+          <!-- End item -->
+          <!-- item -->
+          <div class="card">
+            <div
+              class="card-body col-lg-2 mt-3"
+              style="width: calc((1543px - 40px) / 5)"
+            >
+              <img
+                src="assets/img/card.jpg"
+                class="card-img-top"
+                alt="..."
+                style="height: 300px"
+              />
+              <a class="card-text text-center d-block mt-2">
+                Cốt binh trở lại
+              </a>
+              <div class="d-flex justify-content-between mt-2">
+                <a>Chap 01</a>
+                <span>10 phút trước</span>
+              </div>
+            </div>
+          </div>
+          <!-- End item -->
+          <!-- item -->
+          <div class="card">
+            <div
+              class="card-body col-lg-2 mt-3"
+              style="width: calc((1543px - 40px) / 5)"
+            >
+              <img
+                src="assets/img/card.jpg"
+                class="card-img-top"
+                alt="..."
+                style="height: 300px"
+              />
+              <a class="card-text text-center d-block mt-2">
+                Cốt binh trở lại
+              </a>
+              <div class="d-flex justify-content-between mt-2">
+                <a>Chap 01</a>
+                <span>10 phút trước</span>
+              </div>
+            </div>
+          </div>
+          <!-- End item -->
+          <!-- item -->
+          <div class="card">
+            <div
+              class="card-body col-lg-2 mt-3"
+              style="width: calc((1543px - 40px) / 5)"
+            >
+              <img
+                src="assets/img/card.jpg"
+                class="card-img-top"
+                alt="..."
+                style="height: 300px"
+              />
+              <a class="card-text text-center d-block mt-2">
+                Cốt binh trở lại
+              </a>
+              <div class="d-flex justify-content-between mt-2">
+                <a>Chap 01</a>
+                <span>10 phút trước</span>
+              </div>
+            </div>
+          </div>
+          <!-- End item -->
+        </div>
+        <!-- End list item -->
+      </div>
+    </main>
+    <!-- End #main -->
+  </div>
+</template>
 
 <script>
+import SideBar from "../Sidebar.vue";
+import Header from "../Header.vue";
 export default {
   name: "HomePage",
-  components: {},
+  components: { SideBar, Header },
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
@@ -25,5 +316,18 @@ li {
 
 a {
   color: #42b983;
+}
+</style>
+
+<style>
+.carousel-small,
+.carousel-small .carousel-inner,
+.carousel-small img {
+  height: 500px !important;
+}
+
+.carousel-small .carousel-control-prev-icon::after,
+.carousel-small .carousel-control-next-icon::after {
+  content: "";
 }
 </style>
